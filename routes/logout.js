ceb5d9f37f28a7c3842f9c3efc6c147075723530
finnbear <finnbearone@gmail.com>
2017-09-02 14:11:22 -0700

    Sessions are removed from the database when logged out.

--- routes/logout.js
+++ routes/logout.js
@@ -1,8 +1,13 @@
+var database = require('database');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET logout page. */
 router.get('/', function(req, res, next) {
+    if (req.session.session) {
+        database.deleteSession(req.session.session, function() {});
+    }
     req.session.reset();
     res.redirect('/');
 });
