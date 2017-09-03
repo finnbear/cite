51d06acec37bd573756aa2625075e01712f72e8b
finnbear <finnbearone@gmail.com>
2017-09-02 22:21:11 -0700

    Automatic title significance detection.

--- routes/index.js
+++ routes/index.js
@@ -34,10 +34,61 @@ router.get('/', function(req, res, next) {
                             }
 
                             var processPage = function (page) {
+                                function editDistance(s1, s2) {
+                                    s1 = s1.toLowerCase();
+                                    s2 = s2.toLowerCase();
+
+                                    var costs = new Array();
+                                    for (var i = 0; i <= s1.length; i++) {
+                                        var lastValue = i;
+                                        for (var j = 0; j <= s2.length; j++) {
+                                            if (i == 0)
+                                                costs[j] = j;
+                                            else {
+                                                if (j > 0) {
+                                                    var newValue = costs[j - 1];
+                                                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                                                        newValue = Math.min(Math.min(newValue, lastValue),
+                                                                costs[j]) + 1;
+                                                    costs[j - 1] = lastValue;
+                                                    lastValue = newValue;
+                                                }
+                                            }
+                                        }
+                                        if (i > 0)
+                                            costs[s2.length] = lastValue;
+                                    }
+                                    return costs[s2.length];
+                                }
+
+                                function similarity(s1, s2) {
+                                    var longer = s1;
+                                    var shorter = s2;
+                                    if (s1.length < s2.length) {
+                                        longer = s2;
+                                        shorter = s1;
+                                    }
+                                    var longerLength = longer.length;
+                                    if (longerLength == 0) {
+                                        return 1.0;
+                                    }
+                                    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+                                }
+
                                 var dom = $(page);
 
                                 citation.sourceAuthor = dom.filter("meta[name=author]").attr("content") || "";
-                                citation.sourceContainerTitle = dom.filter("title").text();
+
+                                var domTitle = dom.filter("title").text();
+                                var titleSimilarity = similarity(domTitle, options.host.replace("www", "").replace(".com", "").replace(".org", "").replace(".edu", ""));
+                                console.log(titleSimilarity);
+                                if (titleSimilarity >= 0.30) {
+                                    citation.sourceContainerTitle = domTitle;
+                                    citation.sourceTitle = "";
+                                } else {
+                                    citation.sourceContainerTitle = "";
+                                    citation.sourceTitle = domTitle;
+                                }
 
                                 res.render('index', {login: login, citation: citation});
                             };
