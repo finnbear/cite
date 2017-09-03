c009afa3a0d2036c9a418327d03a9a038b96770d
finnbear <finnbearone@gmail.com>
2017-09-03 15:13:54 -0700

    Removed unecessary middleware.

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use('/login', login);
 app.use('/logout', logout);
 
 // Trust proxy
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     req.connection.proxySecure = true;
     next();
-});
+});*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
